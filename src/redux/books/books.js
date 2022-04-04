c9258776ec963caad74ff643f14b58b9e19ac440
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-04-04 21:03:02 +0430

    add reducers and actions

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,17 +1,34 @@
 // Actions
-const SET = 'todo/SET';
+const ADD = 'bookstore/books/add';
+const DEL = 'bookstore/books/del';
+
+// Empty array of books
+const initialState = [];
 
 // Reducer
-export default function reducer(state = {}, action = {}) {
+export default function booksReducer(state = initialState, action = {}) {
   switch (action.type) {
-    case SET:
-      // Perform action
-      return state;
+    case ADD:
+      return [...state, {
+        ...action.payload,
+      }];
+    case DEL:
+      return state.filter((book) => book.id !== action.id);
     default: return state;
   }
 }
 
 // Action Creators
-export function set() {
-  return { type: SET };
+export function add(book) {
+  return {
+    type: ADD,
+    payload: { book },
+  };
+}
+
+export function del(id) {
+  return {
+    type: DEL,
+    payload: { id },
+  };
 }
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,17 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import createLogger from 'redux-logger';
-import todo from './modules/todo'
+import booksReducer from './books/books';
+import categoriesReducer from './categories/categories';
 
-const loggerMiddleware = createLogger(); // initialize logger
+// initialize logger
+const loggerMiddleware = createLogger();
 
-const createStoreWithMiddleware = applyMiddleware(loggerMiddleware)(createStore); // apply logger to redux
+// apply logger to redux
+const createStoreWithMiddleware = applyMiddleware(loggerMiddleware)(createStore);
 
 const reducer = combineReducers({
-  todo,
+  booksReducer,
+  categoriesReducer,
 });
 
 const configureStore = (initialState) => createStoreWithMiddleware(reducer, initialState);
